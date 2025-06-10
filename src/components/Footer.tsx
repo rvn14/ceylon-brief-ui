c6eccacadba5fc957fcf03e6b2a50e24db12d062
rvn14 <dasunadithya123@gmail.com>
2025-06-10 11:24:28 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='bg-red-900 text-white text-xs md:text-sm w-screen p-4 flex justify-center items-center'>
-        Copyright© 2025 NewsApp. All rights reserved.
+        CeylonBrief@2025. All rights reserved.
     </footer>
   )
 }
