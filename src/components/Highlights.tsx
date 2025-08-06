7885cd8aa80e5c5c16a9c3ec109d1fbf65da4710
rvn14 <dasunadithya123@gmail.com>
2025-08-06 08:15:25 +0530

    fixed some allignments

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -127,7 +127,7 @@ const Highlights = ({ news }: HighlightsProps) => {
         {/* Main Featured Article */}
         <section className='mb-8'>
           <div className='grid grid-cols-1 md:grid-cols-5'>
-            <div className='md:col-span-3 relative aspect-[5/3] w-full overflow-hidden'> 
+            <div className='md:col-span-3 relative aspect-[5/3] w-full overflow-hidden group'> 
                <Image 
                 src={politicSrc}
                 alt="Featured news"
